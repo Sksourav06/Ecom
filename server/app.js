a4c300880005e5867abec55f89f7dba51b063e3d
Sourav <you@example.com>
2024-12-07 20:24:17 +0600

    new changes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 const userrouter = require("./routes/userRouter.js");
+const categoryrouter = require("./routes/categoryRouter.js")
+const attributerouter = require("./routes/attributeRouter.js")
+const productrouter = require("./routes/productRouter.js")
+const variantrouter = require("./routes/variantRouter.js")
+const wishlistrouter = require("./routes/wishlistRouter.js")
+const cartrouter = require("./routes/cartRouter.js")
 const express = require("express");
 const app = express();
 const port = 8000;
@@ -11,8 +17,21 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(express.json());
 require("./Models/usertable.js");
+require("./Models/category");
+require("./Models/attribute");
+require("./Models/product");
+require("./Models/product_variant");
+require("./Models/wishlist");
+require("./Models/cart");
+
 connectdb(database)
 app.use("/api/user", userrouter);
+app.use("/api/category", categoryrouter);
+app.use("/api/attribute", attributerouter);
+app.use("/api/product",productrouter);
+app.use("/api/variant", variantrouter);
+app.use("/api/wishlist", wishlistrouter);
+app.use("/api/cart", cartrouter);
 app.listen(port, () => {
   console.log(`server is runing at ${port}`);
 });
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -5,13 +5,20 @@ const login = require('../Controllers/user/login')
 const usersingle = require('../Controllers/user/usersingle')
 const updateuser = require('../Controllers/user/updateuser')
 const authenticateToken = require('../middlewares/verifytoken')
+const deleteuser = require('../Controllers/user/deleteuser')
+const frontenduser = require('../Controllers/user/frontend/frontend_usersingle')
+const frontendupdateuser = require('../Controllers/user/frontend/frontend_updateuser')
+
 
 const router = express.Router()
 
 router.get('/',userlist)
+router.get('/userinfo',authenticateToken,frontenduser)
+router.get('/:id',usersingle)
+router.delete('/:id',deleteuser)
 router.post('/register',register)
 router.post('/login',login)
-router.get('/:id',usersingle)
+router.patch('/',authenticateToken,frontendupdateuser)
 router.patch('/:id',authenticateToken,updateuser)
 
 
