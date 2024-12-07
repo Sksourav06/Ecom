91340519d2a8b849ee83eaa26e12a84bd46dde57
Sourav <you@example.com>
2024-12-07 18:16:37 +0600

    new changes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,13 +2,17 @@ const express = require('express')
 const userlist = require('../Controllers/user/userlist')
 const register = require('../Controllers/user/register')
 const login = require('../Controllers/user/login')
+const usersingle = require('../Controllers/user/usersingle')
+const updateuser = require('../Controllers/user/updateuser')
+const authenticateToken = require('../middlewares/verifytoken')
 
 const router = express.Router()
 
 router.get('/',userlist)
 router.post('/register',register)
 router.post('/login',login)
-
+router.get('/:id',usersingle)
+router.patch('/:id',authenticateToken,updateuser)
 
 
 module.exports = router
